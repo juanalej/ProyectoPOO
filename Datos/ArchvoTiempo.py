9ba3c1128c01b8eebd3cefae27c0788aebe41e46
camilo <camilorubio723@gmail.com>
2022-05-10 22:55:47 -0500

    archivo datos

--- Datos/ArchvoTiempo.py
+++ Datos/ArchvoTiempo.py
@@ -3,5 +3,7 @@ import pandas as pd
 from ProyectoPOO.Clases.Tiempo import Tiempo
 class ArchivoTiempo:
     Tiempos:Tiempo[3]
+    def __init__(self,tiempos):
+        Tiempo= tiempos
     def GenerarTXT(self):
         pd.DataFrame(Tiempo).to_csv('Tiempos.cvs')
